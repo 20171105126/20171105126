34b2d8bee65e49bd271e6326d6cc789b62a426d8
赵一波 <s20171105126@admindeiMac-12.local>
2018-06-21 09:49:54 +0800

    Hellow world
    熟悉把xcode上的程序上传到Github的操作流程

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  20171105126
+//
+//  Created by 赵一波 on 2018/6/21.
+//  Copyright © 2018年 赵一波. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
