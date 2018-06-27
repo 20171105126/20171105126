f82a10a8392f7f0664098a772ec3c7a759cb76f6
20171105126 <s20171105126@admindeiMac-12.local>
2018-06-27 12:02:54 +0800

    文件数据读取的参考代码
    对于读取Excel文件的操作，使用了文件随机定位函数fseek(),它的一般调用格式如下：
    fseek(文件指针，位移量，起始位置) ;
    fseek()参数说明：
    位移量
    指重新定位时的字节偏移数，表示相对于基址的字符数，通常是一个长整型数，可以是整形常量，整形表达式等。如果用整型常量，需要再后面加上字母“L”；如
    果使用整形表达式需要用“(long)(表达式)”强制转换成长整形。
    起始位置
    指重新定位时的基准点，也就是基址，用整数或符合常量表示。
    文件操作的一般过程
    
    定义文件指针 FILE *
    
    打开文件 fopen
    
    对文件进行读写
    FILE*
    指针作为文件句柄，是文件访问的唯一标识，它由fopen函数创建，fopen打开文件成功，则返回一个有效的FILE*指针，否则返回空指针NULL
    建立数组，将表格中的数据读取到数组中
    对c语言文件部分的内容回去看

---
+++ ibhb.c
@@ -0,0 +1,9 @@
+//
+//  ibhb.c
+//  20171105126
+//
+//  Created by 赵一波 on 2018/6/26.
+//  Copyright © 2018年 赵一波. All rights reserved.
+//
+
+#include "ibhb.h"
---
+++ ibhb.h
@@ -0,0 +1,14 @@
+//
+//  ibhb.h
+//  20171105126
+//
+//  Created by 赵一波 on 2018/6/26.
+//  Copyright © 2018年 赵一波. All rights reserved.
+//
+
+#ifndef ibhb_h
+#define ibhb_h
+
+#include <stdio.h>
+
+#endif /* ibhb_h */
